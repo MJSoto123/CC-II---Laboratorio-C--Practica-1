3f2eec03f5c21845962b8b60efe61bbe8c806af0
londonmuse 48 <jsotobe@unsa.edu.pe>
2021-04-23 00:05:32 -0500

    first commit

---
+++ 013.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+//----------------- STATEMENT -----------------
+
+
+//----------------- SOLVE -----------------
+
+//----------------- FUNCTION -----------------
+
+
+//----------------- MAIN -----------------
+int main(){
+    cout << "work";
+}
